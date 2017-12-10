7916dfd6423ccc60fd8f46dc1505ade19b06e2b9
JiwooPark <jpark830@me.com>
2017-12-09 19:00:08 -0800

    init setup vue

---
+++ onionup/app/javascript/app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: new Date
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
