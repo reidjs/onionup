bfd6fad5f193c6883e4c88351c67e7c8a88d1d26
Tim Kharshan <timkharshan@hotmail.com>
2017-12-10 15:24:15 -0800

    commented out sidekiq config, tested jobs

---
+++ onionup/app/jobs/ping_all_job.rb
@@ -0,0 +1,37 @@
+require 'thread'
+
+class PingAllJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    @sites = Site.all
+
+    #make threadsafe pings queue and finished pings queu. Multithreads didnt like me instantiating Ping.new() inside a thread
+    pings = Queue.new
+    finished_ping = Queue.new
+    @sites.each do |site| 
+      pings << Ping.new
+    end
+    #make new thread for every site: get a new ping, ping the site, apply attributes to ping and put it in finished_ping queue
+    #pretty sure the threadsafe queues are what made this work. 
+    threads = @sites.map do |site|
+      Thread.new do
+          ping = pings.pop
+          ping.site_id = site.id
+          if (site.ping)
+            ping.status = true;
+          else
+            ping.status = false;
+          end
+          finished_ping.push(ping)
+      end
+    end
+    #wait for threads to finish
+    threads.each{|thr| thr.join}    
+    #save every ping
+    while (!finished_ping.empty?)
+      finished_ping.pop(true).save!
+    end
+  end
+end
+# PingAllJob.new.perform("hi")
