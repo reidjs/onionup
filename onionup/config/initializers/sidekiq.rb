ad6a0bb0284d44f52c0a55589d468a55590fdc61
Christopher Bigelow <chrisbigelow25@gmail.com>
2017-12-11 11:43:31 -0800

    remove sidekiq workers

--- onionup/config/initializers/sidekiq.rb
+++ onionup/config/initializers/sidekiq.rb
@@ -1,11 +1,11 @@
 
 
 
-Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379' }
-end
+# Sidekiq.configure_server do |config|
+#   config.redis = { url: 'redis://localhost:6379' }
+# end
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379' }
-end
+# Sidekiq.configure_client do |config|
+#   config.redis = { url: 'redis://localhost:6379' }
+# end
 # redis://localhost:6379/0/
