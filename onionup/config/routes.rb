d2dabe432f657a23c018ec3971344a03266012c5
reid <reidsherman@gmail.com>
2017-12-10 16:22:17 -0800

    Remove devise

--- onionup/config/routes.rb
+++ onionup/config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
       match 'ping', to: 'sites#index_ping', via: [:get]
     end
 
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
 
---
+++ onionup/db/migrate/20171211002044_remove_devise.rb
@@ -0,0 +1,5 @@
+class RemoveDevise < ActiveRecord::Migration[5.1]
+  def change
+    drop_table :users
+  end
+end
