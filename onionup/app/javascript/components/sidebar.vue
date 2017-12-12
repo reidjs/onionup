de60fd82444873c3e872ec1cd2ea641b5dde06db
JiwooPark <jpark830@me.com>
2017-12-12 11:45:14 -0800

    add structure to sidebar

--- onionup/app/javascript/components/sidebar.vue
+++ onionup/app/javascript/components/sidebar.vue
@@ -2,11 +2,38 @@
 <template >
 
       <div v-if="currentUser" class="sidebar">
-        <h1>sidebar</h1>
-        <router-link to="/">Go to /</router-link>
-        <router-link to="/site">go to site</router-link>
-        <router-link to="/login">go to login</router-link>
-        <router-link to="/signup">go to signup</router-link>
+        <div class="sidebar-logo">
+          <p class="logo-image">
+            ONION UP
+          </p>
+        </div>
+        <div>
+          <div>
+            <ul class="dropdown">
+              <li>
+                <router-link to="/">Go to /</router-link>
+              </li>
+              <li>
+                <router-link to="/">Go to /</router-link>
+              </li>
+              <li>
+                <router-link to="/">Go to /</router-link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <router-link to="/site">go to site</router-link>
+          </div>
+          <div>
+            <router-link to="/login">go to login</router-link>
+          </div>
+          <div>
+            <router-link to="/signup">go to signup</router-link>
+          </div>
+          
+          
+          
+        </div>
       </div>
 
 
