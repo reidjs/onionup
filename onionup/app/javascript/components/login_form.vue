ec071eb66e60a720e1159f49a33d7d7d9c129b02
reid <reidsherman@gmail.com>
2017-12-11 18:39:08 -0800

    Create login and signup forms

--- onionup/app/javascript/components/login_form.vue
+++ onionup/app/javascript/components/login_form.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <h1> User login form below:</h1>
+    <ul id="error-list">
+      <li v-for="(error) in errors" :key="error[0]">
+        {{error}}
+      </li>
+    </ul>
     <label>Username: </label><input type="text" v-model="user.username"><br />
     <label>Password: </label><input type="text" v-model="user.password"><br />
     <button v-on:click="login">Log in</button>
@@ -17,7 +22,8 @@
         user: {
           username: 'username',
           password: 'password'
-        }
+        },
+        errors: []
       }
     },
     methods:{
@@ -36,7 +42,8 @@
         })
         .catch(e => {
           console.log(e);
-          this.errors.push(e)
+          // debugger
+          this.errors.push(e.response.data[0])
         })
       },
     },
