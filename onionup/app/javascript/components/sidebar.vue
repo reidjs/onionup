562f4d0bf144b4134a57d58ce22c7b94419c1b13
Tim Kharshan <timkharshan@hotmail.com>
2017-12-11 21:43:29 -0800

    signup/login redirecting

--- onionup/app/javascript/components/sidebar.vue
+++ onionup/app/javascript/components/sidebar.vue
@@ -4,8 +4,9 @@
       <div v-if="currentUser" class="sidebar">
         <h1>sidebar</h1>
         <router-link to="/">Go to /</router-link>
-        <router-link to="/login">login</router-link>
-        <router-link to="/signup">signup</router-link>
+        <router-link to="/site">go to site</router-link>
+        <router-link to="/login">go to login</router-link>
+        <router-link to="/signup">go to signup</router-link>
       </div>
 
 
