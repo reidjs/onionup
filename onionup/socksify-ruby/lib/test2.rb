dc59b964f2f266e8f161a6521e923a4481c5a3a3
Christopher Bigelow <chrisbigelow25@gmail.com>
2017-12-12 18:10:34 -0800

    trim file structure

--- onionup/socksify-ruby/lib/test2.rb
+++
@@ -1,5 +0,0 @@
-require 'socksify/http'
-uri = URI.parse('http://answerstedhctbek.onion/')
-Net::HTTP.SOCKSProxy(ENV['TOR_IP'], 9050).start(uri.host, uri.port) do |http|
-  p http.get(uri.path)
-end
