83e9784e3f27c2a39b8f521356922a0c438b20fc
reid <reidsherman@gmail.com>
2017-12-13 10:08:19 -0800

    Add demo user on backend

--- onionup/app/javascript/app.vue
+++ onionup/app/javascript/app.vue
@@ -19,7 +19,6 @@
   import Vue from 'vue';
   import VueRouter from 'vue-router';
   import SessionForm from './components/session_form'
-  import Sidebar from './components/sidebar'
   import IndexComponent from './components/index_component'
   import SiteShowComponent from './components/site_show_component';
   import LoginForm from './components/login_form'
--- onionup/db/seeds.rb
+++ onionup/db/seeds.rb
@@ -9,7 +9,12 @@ user1 = User.new(
   username: 'asdfasdf',
   password: 'asdfasdf'
 )
+demo_user = User.new(
+  username: 'DemoUser',
+  password: 'asdfasdf'
+)
 user1.save!
+demo_user.save!
 
 site1 = Site.new(
   url:  URI.parse('http://answerstedhctbek.onion/'),
@@ -19,6 +24,27 @@ site2 = Site.new(
   url: URI.parse('http://google.com'),
   user_id: user1.id
 )
+hidden_nytimes = Site.new(
+  url: URI.parse('https://www.nytimes3xbfgragh.onion/'),
+  user_id: demo_user.id
+)
+hidden_facebook = Site.new(
+  url: URI.parse('https://facebookcorewwwi.onion/'),
+  user_id: demo_user.id
+)
+hidden_propublica = Site.new(
+  url: URI.parse('https://www.propub3r6espa33w.onion/'),
+  user_id: demo_user.id
+)
+hidden_duckduckgo = Site.new(
+  url: URI.parse('http://3g2upl4pq6kufc4m.onion/'),
+  user_id: demo_user.id
+)
 
 site1.save!
 site2.save!
+
+hidden_nytimes.save!
+hidden_facebook.save!
+hidden_propublica.save!
+hidden_duckduckgo.save!
