7c40919fb53d968fbf4d06b4e872fb4507c7104d
reid <reidsherman@gmail.com>
2017-12-13 09:37:12 -0800

    Duplicate usernames validated at model and db level fixes #12

--- onionup/app/models/user.rb
+++ onionup/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   validates :username, :password_digest, :session_token, presence: true
   validates :password, length: { minimum: 6, allow_nil: true }
+  validates :username, uniqueness: true
   
   attr_reader :password
   
