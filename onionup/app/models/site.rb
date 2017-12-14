49b7c5386313fc51890d5df13bf02bfce88f4046
Tim Kharshan <timkharshan@hotmail.com>
2017-12-13 17:08:57 -0800

    going to bug fixes, will come back

--- onionup/app/models/site.rb
+++ onionup/app/models/site.rb
@@ -6,13 +6,30 @@ class Site < ApplicationRecord
 
   def ping
     uri = URI.parse(self.url)
+
     # if !uri.host.nil? && uri.path
       begin
-        puts "started socks request!"
+        puts "started socks request!  id: "
+        p self.id
         Net::HTTP.SOCKSProxy(ENV['TOR_IP'], 9050).start(uri.host, uri.port) do |http|
+          p http
+          a = Time.now
+          
+          begin
+           res = Net::HTTP.SOCKSProxy(ENV['TOR_IP'], 9050).get_response(uri)
+            p res.body
+            p 'succes get' 
+            p self.id 
+          rescue
+            p 'no get ' 
+            p self.id
+          end
+          b = Time.now
+          p b-a
+
           return true
         end
-          # p http.get(uri.path)
+          # p 
         # return true
       rescue
         puts "Host unreachable error"
