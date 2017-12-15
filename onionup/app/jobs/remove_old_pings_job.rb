7a1ceb52c891b556105a45e7ae33f2e8d590bce2
Tim Kharshan <timkharshan@hotmail.com>
2017-12-15 12:59:48 -0800

    jobs fix

--- onionup/app/jobs/remove_old_pings_job.rb
+++ onionup/app/jobs/remove_old_pings_job.rb
@@ -1,8 +1,8 @@
 class RemoveOldPingsJob < ApplicationJob
   queue_as :default
 
-  def perform(OldestPermitted)
-    time = DateTime.now-OldestPermitted
+  def perform(oldestPermitted)
+    time = DateTime.now-oldestPermitted
     oldPings = Ping.where("created_at <= ?",time)
     oldPings.each{|ping| ping.delete}
   end
